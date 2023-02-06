364cdd2a75b6cea2ac7aeadf8533db0a54f237b8
kereyesc <102633532+kereyesc@users.noreply.github.com>
2023-02-06 12:48:12 -0500

    Added Authentication to LoginActivity to allow individuals to log in.

--- app/src/main/java/com/example/employee_nexus/LoginActivity.java
+++ app/src/main/java/com/example/employee_nexus/LoginActivity.java
@@ -10,6 +10,11 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,6 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText password;
     private Button login;
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
         password = findViewById(R.id.loginPassword);
         login = findViewById(R.id.loginButton);
 
+        mAuth = FirebaseAuth.getInstance();
 
         login.setOnClickListener(new View.OnClickListener(){
           @Override
@@ -39,8 +46,20 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void loginUser(String eText, String pText) {
+
+        mAuth.signInWithEmailAndPassword(eText, pText).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
                 Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
                 finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "Login Failed, Invalid email or password", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
