52d5f01063753bdb75cae68f493f07dae8ec8ea2
kereyesc <102633532+kereyesc@users.noreply.github.com>
2023-02-13 15:03:23 -0500

    Added new fragments to nav bar: Clock-In & Schedule Empty Layout ready for Clock-In

---
+++ app/src/main/java/com/example/employee_nexus/ui/clockIn/ClockInViewModel.java
@@ -0,0 +1,19 @@
+package com.example.employee_nexus.ui.clockIn;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ClockInViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public ClockInViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Clock-In fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
